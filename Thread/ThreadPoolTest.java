5d91d41c6093347f098f4db3894aada1726a1baf
newbee-D <179294162@qq.com>
2019-05-09 15:37:25 +0800

    change ThreadPoolTest

--- Thread/ThreadPoolTest.java
+++ Thread/ThreadPoolTest.java
@@ -2,28 +2,30 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 class ThreadPoolTest 
 {
-	public static volatile int j=0;  //同一时刻只允许一个线程修改其值
-	public static volatile boolean exit=false;
+	//public static volatile int j=0;  //同一时刻只允许一个线程修改其值
+	//public static volatile boolean exit=false;
 	public static void main(String[] args) 
 	{
-
 		ExecutorService threadPool=Executors.newFixedThreadPool(10);
-		while(!exit){
-			new Thread(new Runnable(){
-				@Override
-				public void run(){
-					j=j+1;
-					System.out.println("线程"+Thread.currentThread().getName()+"正在执行"+j);
-					
-					try{
-						
-						Thread.sleep(9000);
-					}catch(InterruptedException e){
-						e.printStackTrace();
-					}finally{
-					}
-				}
-			}).start();
+		for(int i=0;i<20;i++){
+			threadPool.execute(new MyThread(i)); //提交任务
+		}
+	}
+
+	public static class MyThread extends Thread
+	{
+		private int num;
+		public MyThread(int num){
+			this.num=num;
+		}
+		public void run(){
+			try{
+				System.out.println("Thread "+this.num+" is running");
+				Thread.sleep(3000);
+				System.out.println("Thread "+this.num+" is dead");
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
 		}
 	}
 }
